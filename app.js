0d9ea53c5ddd73ab7d489e8897427adff24d6770
Djengua <dav_vd@hotmail.com>
2015-07-31 13:16:31 -0500

    first example

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express'),
+  path = require('path'),
+  app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.engine('html', require('jade').__express);
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/', function(req, res){
+  res.render('index.jade', {
+    title: 'Healt Center'
+  });
+});
+/*
+app.get('/platforms', function(req, res){
+  res.render('platforms', {
+    title: 'Platforms',
+    platforms: platforms
+  });
+});*/
 app.listen(3000);
+console.log('Express server listening on port 3000');
