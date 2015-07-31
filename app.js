e2c4621fe2dfbc5f5cfe8b4b146316cdaa5dde1b
Djengua <dav_vd@hotmail.com>
2015-07-31 14:03:07 -0500

    fix

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/', function(req, res){
   res.render('index.jade', {
     title: 'Healt Center'
